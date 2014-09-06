1c8665e40feea1a628cea9e07b391b70e581d205
Omar Aguirre <omersguchigu@gmail.com>
2014-09-05 22:31:33 -0500

    change gitignore and setting new endpoints

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -2,7 +2,7 @@
 
 angular.module('angularAppApp')
   .controller('UserFinderCtrl', function ($scope, $http) {
-    $http.get('http://d7.demo.pocketlab.mx/demo/user').
+    $http.get('http://headlessd7-pocketlab.rhcloud.com/api/user-finder').
     success(function(data){
       $scope.users = data;
     });
@@ -25,7 +25,7 @@ angular.module('angularAppApp')
   })
 
   .controller('UserDetailCtrl', function ($scope, $http, $routeParams) {
-    var uriUser = 'http://d7.demo.pocketlab.mx/demo/user/'+$routeParams.uid;
+    var uriUser = 'http://headlessd7-pocketlab.rhcloud.com/api/user/'+$routeParams.uid;
     $http.get(uriUser).
     success(function(data){
       $scope.user = data;
